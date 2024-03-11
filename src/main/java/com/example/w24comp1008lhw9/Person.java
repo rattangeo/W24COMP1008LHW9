e188a01ad0d30deb54b8335f42302f28a4f40deb
Rattan Ashat <rashat@lakeheadu.ca>
2024-03-11 13:40:23 -0400

    Person Class updated

--- src/main/java/com/example/w24comp1008lhw9/Person.java
+++ src/main/java/com/example/w24comp1008lhw9/Person.java
@@ -1,2 +1,80 @@
-package com.example.w24comp1008lhw9;public class Person {
+package com.example.w24comp1008lhw9;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+
+    private String firstName, lastName, address;
+    private LocalDate birthday;
+
+    public Person(String firstName, String lastName, String address, LocalDate birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.birthday = birthday;
+    }
+
+    /**
+     * Format Names to remove leading and trailing white spaces and Capitalize the first Letter
+     * @return
+     */
+    private String formatName(String name){
+        name = name.trim();
+        if (name.length()>1)
+            return name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        return name.substring(0,1).toUpperCase();
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        firstName = formatName(firstName);
+        if (firstName.length()>=2)
+            this.firstName=firstName;
+        else
+            throw new IllegalArgumentException("First Name must be at least 2 characters long");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        lastName=formatName(lastName);
+        if (lastName.length()>=2)
+            this.lastName = lastName;
+        else
+            throw new IllegalArgumentException("Last name must be at least 2 characters long");
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        if (birthday.isAfter(LocalDate.now()))
+            throw new IllegalArgumentException("Birthday cannot be in the future");
+        this.birthday = birthday;
+    }
+
+    /**
+     * Returns the age of the person in years
+     */
+    public int getAge(){
+        //return LocalDate.now().getYear() - birthday.getYear();
+        return Period.between(birthday,LocalDate.now()).getYears();
+    }
+
 }
